20c1fc50db5e3033d777ece9cb59abe2d45b5041
Christina Bogner <christina.bogner@uni-bayreuth.de>
2021-05-15 21:13:20 +0200

    erstes tutorial läuft als paket

--- Create_and_update.R
+++ Create_and_update.R
@@ -4,3 +4,9 @@ usethis::create_package('/media/Daten/Chrissies Dateien/Lehre/Material/GitHub/RU
 
 # create tutorial
 usethis::use_tutorial("Explorative_Datenanalyse", "Explorative Datenanalyse", open = interactive())
+
+usethis::use_gpl3_license('Christina Bogner')
+
+devtools::check()
+
+devtools::build()
