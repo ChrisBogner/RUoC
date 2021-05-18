d4af51b789e7089e5bc8c6d616af63ef7c001f7e
Christina Bogner <christina.bogner@uni-bayreuth.de>
2021-05-18 19:33:10 +0200

    Update Zufall

--- Create_and_update.R
+++ Create_and_update.R
@@ -3,10 +3,17 @@
 usethis::create_package('/media/Daten/Chrissies Dateien/Lehre/Material/GitHub/RUoC')
 
 # create tutorial
-usethis::use_tutorial("Explorative_Datenanalyse", "Explorative Datenanalyse", open = interactive())
+usethis::use_tutorial("Zufall", "Zufall in der Statistik", open = interactive())
 
-usethis::use_gpl3_license('Christina Bogner')
+#usethis::use_gpl3_license('Christina Bogner')
 
 devtools::check()
 
 devtools::build()
+
+usethis::use_package('emo')
+#usethis::use_package('tidyverse')
+usethis::use_package('kableExtra')
+
+# run
+learnr::run_tutorial('Zufall', package = 'RUoC')
